c732bdfafe3ac2800b7eb2315d9d1b38a087cc01
AlexAbramoff <alex93178@gmail.com>
2021-11-04 20:14:49 +0300

    feat: complete task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+// const { clear } = require('console');
+const copyFolder = path.join(__dirname, 'files-copy');
+const sourceDir = path.join(__dirname, 'files');
+fs.mkdir(copyFolder, { recursive: true }, err => {
+  if (err) {
+    throw err;
+  }
+  clearDirectory();
+  fs.readdir(sourceDir, 'utf-8', (err, files) => {
+    if (err) {
+      throw err;
+    }
+    files.forEach(file => {
+      fs.copyFile(path.join(sourceDir, file), path.join(copyFolder, file), err => {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+  });
+});
 
+function clearDirectory() {
+  fs.readdir(copyFolder, 'utf-8', (err, files) => {
+    if (err) {
+      throw err;
+    }
+    files.forEach(file => {
+      fs.unlink(path.join(copyFolder, file), err => {
+        if (err) {
+          throw err;
+        }
+      });
+    });
+  });
+}
